c7fbc9f2302b85af9677c3462a06aa74683a2703
TJ Szewczak <tj.dividemysky@gmail.com>
2024-08-02 17:03:59 -0600

    clean up admin

--- acf-cloudflare-stream/class-nsz-acf-field-cloudflare-stream.php
+++ acf-cloudflare-stream/class-nsz-acf-field-cloudflare-stream.php
@@ -4,126 +4,126 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+    exit;
 }
 
 /**
  * nsz_acf_field_cloudflare_stream class.
  */
 class nsz_design_video_field_acf_field_cloudflare_stream extends \acf_field {
-	/**
-	 * Controls field type visibilty in REST requests.
-	 *
-	 * @var bool
-	 */
-	public $show_in_rest = true;
+    /**
+     * Controls field type visibilty in REST requests.
+     *
+     * @var bool
+     */
+    public $show_in_rest = true;
 
-	/**
-	 * Environment values relating to the theme or plugin.
-	 *
-	 * @var array $env Plugin or theme context such as 'url' and 'version'.
-	 */
-	private $env;
+    /**
+     * Environment values relating to the theme or plugin.
+     *
+     * @var array $env Plugin or theme context such as 'url' and 'version'.
+     */
+    private $env;
 
-	/**
-	 * Constructor.
-	 */
-	public function __construct() {
-		/**
-		 * Field type reference used in PHP and JS code.
-		 *
-		 * No spaces. Underscores allowed.
-		 */
-		$this->name = 'cloudflare_stream';
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        /**
+         * Field type reference used in PHP and JS code.
+         *
+         * No spaces. Underscores allowed.
+         */
+        $this->name = 'cloudflare_stream';
 
-		/**
-		 * Field type label.
-		 *
-		 * For public-facing UI. May contain spaces.
-		 */
-		$this->label = __( 'Cloudflare Stream', 'cloudflare-stream' );
+        /**
+         * Field type label.
+         *
+         * For public-facing UI. May contain spaces.
+         */
+        $this->label = __( 'Cloudflare Stream', 'cloudflare-stream' );
 
-		/**
-		 * The category the field appears within in the field type picker.
-		 */
-		$this->category = 'content'; // basic | content | choice | relational | jquery | layout | CUSTOM GROUP NAME
+        /**
+         * The category the field appears within in the field type picker.
+         */
+        $this->category = 'content'; // basic | content | choice | relational | jquery | layout | CUSTOM GROUP NAME
 
-		/**
-		 * Field type Description.
-		 *
-		 * For field descriptions. May contain spaces.
-		 */
-		$this->description = __( 'This field uploads videos to Cloudflare stream and provide a video URL.', 'cloudflare-stream' );
+        /**
+         * Field type Description.
+         *
+         * For field descriptions. May contain spaces.
+         */
+        $this->description = __( 'This field uploads videos to Cloudflare stream and provide a video URL.', 'cloudflare-stream' );
 
-		/**
-		 * Field type Doc URL.
-		 *
-		 * For linking to a documentation page. Displayed in the field picker modal.
-		 */
-		$this->doc_url = '';
+        /**
+         * Field type Doc URL.
+         *
+         * For linking to a documentation page. Displayed in the field picker modal.
+         */
+        $this->doc_url = '';
 
-		/**
-		 * Field type Tutorial URL.
-		 *
-		 * For linking to a tutorial resource. Displayed in the field picker modal.
-		 */
-		$this->tutorial_url = '';
+        /**
+         * Field type Tutorial URL.
+         *
+         * For linking to a tutorial resource. Displayed in the field picker modal.
+         */
+        $this->tutorial_url = '';
 
-		/**
-		 * Defaults for your custom user-facing settings for this field type.
-		 */
+        /**
+         * Defaults for your custom user-facing settings for this field type.
+         */
         /*
 		$this->defaults = array(
 			'font_size'	=> 14,
 		);
         */
 
-		/**
-		 * Strings used in JavaScript code.
-		 *
-		 * Allows JS strings to be translated in PHP and loaded in JS via:
-		 *
-		 * ```js
-		 * const errorMessage = acf._e("cloudflare_stream", "error");
-		 * ```
-		 */
-		$this->l10n = array(
-			'error'	=> __( 'Error! Please enter a higher value', 'cloudflare-stream' ),
-		);
+        /**
+         * Strings used in JavaScript code.
+         *
+         * Allows JS strings to be translated in PHP and loaded in JS via:
+         *
+         * ```js
+         * const errorMessage = acf._e("cloudflare_stream", "error");
+         * ```
+         */
+        $this->l10n = array(
+            'error'	=> __( 'Error! Please enter a higher value', 'cloudflare-stream' ),
+        );
 
-		$protocol = is_ssl() ? 'https://' : 'http://';
+        $protocol = is_ssl() ? 'https://' : 'http://';
         $domainName = $_SERVER['HTTP_HOST'] ?? 'localhost';
 
-		$this->env = array(
-			'url'     => $protocol.$domainName.'/app/plugins/nsz-design-video-field/acf-cloudflare-stream/', // URL to the acf-cloudflare-stream directory.
-			'version' => '1.0', // Replace this with your theme or plugin version constant.
-		);
+        $this->env = array(
+            'url'     => $protocol.$domainName.'/app/plugins/nsz-design-video-field/acf-cloudflare-stream/', // URL to the acf-cloudflare-stream directory.
+            'version' => '1.0', // Replace this with your theme or plugin version constant.
+        );
 
-		/**
-		 * Field type preview image.
-		 *
-		 * A preview image for the field type in the picker modal.
-		 */
-		$this->preview_image = $this->env['url'] . '/assets/images/field-preview-custom.png';
+        /**
+         * Field type preview image.
+         *
+         * A preview image for the field type in the picker modal.
+         */
+        $this->preview_image = $this->env['url'] . '/assets/images/field-preview-custom.png';
 
         $this->input_admin_enqueue_scripts();
 
-		parent::__construct();
-	}
+        parent::__construct();
+    }
 
-	/**
-	 * Settings to display when users configure a field of this type.
-	 *
-	 * These settings appear on the ACF “Edit Field Group” admin page when
-	 * setting up the field.
-	 *
-	 * @param array $field
-	 * @return void
-	 */
-	public function render_field_settings( $field ) {
-		/*
-		 * Repeat for each setting you wish to display for this field type.
-		 */
+    /**
+     * Settings to display when users configure a field of this type.
+     *
+     * These settings appear on the ACF “Edit Field Group” admin page when
+     * setting up the field.
+     *
+     * @param array $field
+     * @return void
+     */
+    public function render_field_settings( $field ) {
+        /*
+         * Repeat for each setting you wish to display for this field type.
+         */
         /*
 		acf_render_field_setting(
 			$field,
@@ -137,21 +137,21 @@ class nsz_design_video_field_acf_field_cloudflare_stream extends \acf_field {
 		);
         */
 
-		// To render field settings on other tabs in ACF 6.0+:
-		// https://www.advancedcustomfields.com/resources/adding-custom-settings-fields/#moving-field-setting
-	}
+        // To render field settings on other tabs in ACF 6.0+:
+        // https://www.advancedcustomfields.com/resources/adding-custom-settings-fields/#moving-field-setting
+    }
 
-	/**
-	 * HTML content to show when a publisher edits the field on the edit screen.
-	 *
-	 * @param array $field The field settings and values.
-	 * @return void
-	 */
-	public function render_field( $field ) {
-		// Debug output to show what field data is available.
-		//echo '<pre>';
-		//print_r( $field );
-		//echo '</pre>';
+    /**
+     * HTML content to show when a publisher edits the field on the edit screen.
+     *
+     * @param array $field The field settings and values.
+     * @return void
+     */
+    public function render_field( $field ) {
+        // Debug output to show what field data is available.
+        //echo '<pre>';
+        //print_r( $field );
+        //echo '</pre>';
 
         $api_token = get_option('nsz_cfstream_api_key');
         $account_id = get_option('nsz_cfstream_account_id');
@@ -212,24 +212,26 @@ class nsz_design_video_field_acf_field_cloudflare_stream extends \acf_field {
                         <img class="cloudflare-video-thumbnail-preview" src="<?php echo $thumbnail ?>" />
                     </div>
 
-                    <div class="wrap-item wrap-hls">
-                        <div class="acf-label"><label>HLS Manifest</label></div>
-                        <input  class="data-hls" type="text" name="<?php echo esc_attr($field['name']) ?>[hls]" value="<?php echo $hls ?>"/>
-                    </div>
+                    <div class="cloudflare-video-details-item-holder">
+                        <div class="wrap-item wrap-hls">
+                            <div class="acf-label"><label>HLS Manifest</label></div>
+                            <input  class="data-hls" type="text" name="<?php echo esc_attr($field['name']) ?>[hls]" value="<?php echo $hls ?>"/>
+                        </div>
 
-                    <div class="wrap-item wrap-dash">
-                        <div class="acf-label"><label>Dash Manifest</label></div>
-                        <input  class="data-dash" type="text" name="<?php echo esc_attr($field['name']) ?>[dash]" value="<?php echo $dash ?>" />
-                    </div>
+                        <div class="wrap-item wrap-dash">
+                            <div class="acf-label"><label>Dash Manifest</label></div>
+                            <input  class="data-dash" type="text" name="<?php echo esc_attr($field['name']) ?>[dash]" value="<?php echo $dash ?>" />
+                        </div>
 
-                    <div class="wrap-item wrap-thumbnail">
-                        <div class="acf-label"><label>Thumbnail</label></div>
-                        <input class="data-thumbnail" type="text" name="<?php echo esc_attr($field['name']) ?>[thumbnail]" value="<?php echo $thumbnail ?>" />
-                    </div>
+                        <div class="wrap-item wrap-thumbnail">
+                            <div class="acf-label"><label>Thumbnail</label></div>
+                            <input class="data-thumbnail" type="text" name="<?php echo esc_attr($field['name']) ?>[thumbnail]" value="<?php echo $thumbnail ?>" />
+                        </div>
 
-                    <div class="wrap-item wrap-preview">
-                        <div class="acf-label"> <label>Preview</label></div>
-                        <input class="data-preview" type="text" name="<?php echo esc_attr($field['name']) ?>[preview]" value="<?php echo $preview ?>" />
+                        <div class="wrap-item wrap-preview">
+                            <div class="acf-label"> <label>Preview</label></div>
+                            <input class="data-preview" type="text" name="<?php echo esc_attr($field['name']) ?>[preview]" value="<?php echo $preview ?>" />
+                        </div>
                     </div>
 
                     <div class="wrap-item cloudflare-video-clear-wrapper">
@@ -246,18 +248,18 @@ class nsz_design_video_field_acf_field_cloudflare_stream extends \acf_field {
         }
 
 
-	}
+    }
 
-	/**
-	 * Enqueues CSS and JavaScript needed by HTML in the render_field() method.
-	 *
-	 * Callback for admin_enqueue_script.
-	 *
-	 * @return void
-	 */
-	public function input_admin_enqueue_scripts() {
-		$url     = trailingslashit( $this->env['url'] );
-		$version = $this->env['version'];
+    /**
+     * Enqueues CSS and JavaScript needed by HTML in the render_field() method.
+     *
+     * Callback for admin_enqueue_script.
+     *
+     * @return void
+     */
+    public function input_admin_enqueue_scripts() {
+        $url     = trailingslashit( $this->env['url'] );
+        $version = $this->env['version'];
 
         wp_register_script(
             'nsz-cloudflare-stream-tus',
@@ -266,23 +268,23 @@ class nsz_design_video_field_acf_field_cloudflare_stream extends \acf_field {
             $version
         );
 
-		wp_register_script(
-			'nsz-cloudflare-stream',
-			"{$url}assets/js/field.js",
-			array( 'acf-input' ),
-			$version
-		);
+        wp_register_script(
+            'nsz-cloudflare-stream',
+            "{$url}assets/js/field.js",
+            array( 'acf-input' ),
+            $version
+        );
 
-		wp_register_style(
-			'nsz-cloudflare-stream',
-			"{$url}assets/css/field.css",
-			array( 'acf-input' ),
-			$version
-		);
+        wp_register_style(
+            'nsz-cloudflare-stream',
+            "{$url}assets/css/field.css",
+            array( 'acf-input' ),
+            $version
+        );
 
-		wp_enqueue_script( 'nsz-cloudflare-stream-tus' );
-		wp_enqueue_script( 'nsz-cloudflare-stream' );
-		wp_enqueue_style( 'nsz-cloudflare-stream' );
+        wp_enqueue_script( 'nsz-cloudflare-stream-tus' );
+        wp_enqueue_script( 'nsz-cloudflare-stream' );
+        wp_enqueue_style( 'nsz-cloudflare-stream' );
 
         $api_token = get_option('nsz_cfstream_api_key');
         $account_id = get_option('nsz_cfstream_account_id');
@@ -295,5 +297,5 @@ class nsz_design_video_field_acf_field_cloudflare_stream extends \acf_field {
         );
 
         wp_localize_script( 'nsz-cloudflare-stream', 'nsz_cloudflare_stream', $params );
-	}
+    }
 }
